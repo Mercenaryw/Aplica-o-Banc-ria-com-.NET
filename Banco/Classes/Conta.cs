42142c34dcabef1c1f87469267db39133b8ac37d
Mercenaryw <mercenaryw@gmail.com>
2021-05-16 03:03:53 -0300

    aplicação com .net

---
+++ Banco/Classes/Conta.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Banco
+{
+   public class Conta
+    {
+        //Atributos
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        //Metodo construtor
+        public Conta(TipoConta tipoConta,double saldo, double credito,string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+        //Metodo Sacar
+        public bool Sacar(double valorSaque)
+        {
+            //Validação de saldo suficiente
+            if (this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                Console.WriteLine("Saldo Insuficiente!");
+                return false;
+            }
+            this.Saldo = -valorSaque;
+
+            //https://docs.microsoft.com/pt-br/dotnet/standard/base-types/composite-formatting
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            return true;
+        }
+        //Metodo Depositar
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+        //Metodo Transferir
+        public void Transferir(double valorTranferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTranferencia))
+            {
+             contaDestino.Depositar(valorTranferencia);
+            }
+        }
+        //Metodo toString
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo R$ " + this.Saldo + " | ";
+            retorno += "Crédito R$ " + this.Credito;
+            return retorno;
+        }
+
+    }
+}
